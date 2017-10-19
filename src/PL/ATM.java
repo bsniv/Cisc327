89189c9bc5222c659cae8e807a6debd79ec95075
Caleb Aikens <zollqir@gmail.com>
2017-10-19 15:04:11 -0400

    ATM.java now works basically completely. However, there is basically no error checking.

--- src/PL/ATM.java
+++ src/PL/ATM.java
@@ -1,6 +1,8 @@
 package PL;
 
 import java.util.LinkedList;
+import java.io.*;
+import java.util.Scanner;
 
 import DAL.Data;
 import SharedClasses.Transaction;
@@ -14,23 +16,24 @@ public class ATM {
 		Data data = new Data();
 		data.initUserFile();
 		LinkedList<Transaction> list = new LinkedList<Transaction>(); //store user inputs in list
+		Scanner s = new Scanner(System.in);
 		
-		while (loggedIn == false) //wait until the user logins before accepting any other input
+		while (!loggedIn) //wait until the user logins before accepting any other input
 		{
-			System.out.print("Please login:");
-			String input = System.console().readLine();
-			if (input == "login")
+			System.out.println("Please login:");
+			String input = s.nextLine();
+			if (input.equals("login"))
 			{
-				while (loggedIn == false)
+				while (!loggedIn)
 				{
-					System.out.print("Enter 'ATM' for a normal session, or 'agent' for an agent session:");
-					input = System.console().readLine();
-					if (input == "ATM")
+					System.out.println("Enter 'ATM' for a normal session, or 'agent' for an agent session:");
+					input = s.nextLine();
+					if (input.equals("ATM"))
 					{
 						loggedIn = true;
 						isAgent = false;
 					}
-					else if (input == "agent")
+					else if (input.equals("agent"))
 					{
 						loggedIn = true;
 						isAgent = true;
@@ -38,9 +41,11 @@ public class ATM {
 			}
 		}
 			
-		while (loggedIn == true)
+		while (loggedIn)
 		{
-			if (input == "logout") //upon logout, write EOS and write the Transactions file
+			System.out.print("Please enter a command from the following list:\nlogout\ncreateacct\ndeleteacct\ndeposit\nwithdraw\ntransfer\n");
+			input = s.nextLine();
+			if (input.equals("logout")) //upon logout, write EOS and write the Transactions file
 			{
 				loggedIn = false;
 				list.add(new Transaction(TransactionCodes.EOS, "000", "000", "000", "***"));
@@ -48,86 +53,86 @@ public class ATM {
 				System.exit(0);
 			}
 			
-			if (input == "createacct") //Creating account
+			if (input.equals("createacct")) //Creating account
 			{
 				if (isAgent)
 				{
-					System.out.print("Enter the new account's number:"); //ask for the account number
-					String accountNumber = System.console().readLine();
+					System.out.println("Enter the new account's number:"); //ask for the account number
+					String accountNumber = s.nextLine();
 					
-					System.out.print("Enter the new account's name:"); //ask for the account name
-					String accountName = System.console().readLine();
+					System.out.println("Enter the new account's name:"); //ask for the account name
+					String accountName = s.nextLine();
 					
 					list.add(new Transaction(TransactionCodes.NEW, accountNumber, "000", "000", accountName));
 				}
 				else
-					System.out.print("Sorry, that is a privileged command");
+					System.out.println("Sorry, that is a privileged command");
 			}
 			
-			if (input == "deleteacct") //Deleting account
+			if (input.equals("deleteacct")) //Deleting account
 			{
 				if (isAgent)
 				{
-					System.out.print("Enter the account's number:"); //ask for the account number
-					String accountNumber = System.console().readLine();
+					System.out.println("Enter the account's number:"); //ask for the account number
+					String accountNumber = s.nextLine();
 					
-					System.out.print("Enter the account's name:"); //ask for the account name
-					String accountName = System.console().readLine();
+					System.out.println("Enter the account's name:"); //ask for the account name
+					String accountName = s.nextLine();
 					
 					list.add(new Transaction(TransactionCodes.DEL, accountNumber, "000", "000", accountName));
 				}
 				else
-					System.out.print("Sorry, that is a privileged command");
+					System.out.println("Sorry, that is a privileged command");
 			}
 			
-			if (input == "deposit") //Depositing money
+			if (input.equals("deposit")) //Depositing money
 			{
-				System.out.print("Enter the account's number:"); //ask for the account number
-				String accountNumber = System.console().readLine();
+				System.out.println("Enter the account's number:"); //ask for the account number
+				String accountNumber = s.nextLine();
 				
-				System.out.print("Enter the amount to be deposited"); //ask for the deposited amount
-				String amount = System.console().readLine();
+				System.out.println("Enter the amount to be deposited"); //ask for the deposited amount
+				String amount = s.nextLine();
 				
-				if (Integer.parseInt(amount) > 100000 && isAgent == false)
-					System.out.print("Sorry, only agents can deposit more than $1000.00 at once");
-				else if (Integer.parseInt(amount) > 99999999 && isAgent == true)
-					System.out.print("Sorry, not event agents can deposit more than $999,999.99 at once");
+				if (Integer.parseInt(amount) > 100000 && !isAgent)
+					System.out.println("Sorry, only agents can deposit more than $1000.00 at once");
+				else if (Integer.parseInt(amount) > 99999999 && isAgent)
+					System.out.println("Sorry, not event agents can deposit more than $999,999.99 at once");
 				else
 					list.add(new Transaction(TransactionCodes.DEP, accountNumber, amount, "000", "***"));
 			}
 			
-			if (input == "withdraw") //Withdrawing money
+			if (input.equals("withdraw")) //Withdrawing money
 			{
-				System.out.print("Enter the account's number:"); //ask for the account number
-				String accountNumber = System.console().readLine();
+				System.out.println("Enter the account's number:"); //ask for the account number
+				String accountNumber = s.nextLine();
 				
-				System.out.print("Enter the amount to be deposited"); //ask for the deposited amount
-				String amount = System.console().readLine();
+				System.out.println("Enter the amount to be deposited"); //ask for the deposited amount
+				String amount = s.nextLine();
 				
-				if (Integer.parseInt(amount) > 100000 && isAgent == false)
-					System.out.print("Sorry, only agents can withdraw more than $1000.00 at once");
-				else if (Integer.parseInt(amount) > 99999999 && isAgent == true)
-					System.out.print("Sorry, not event agents can withdraw more than $999,999.99 at once");
+				if (Integer.parseInt(amount) > 100000 && !isAgent)
+					System.out.println("Sorry, only agents can withdraw more than $1000.00 at once");
+				else if (Integer.parseInt(amount) > 99999999 && isAgent)
+					System.out.println("Sorry, not event agents can withdraw more than $999,999.99 at once");
 				else
 					list.add(new Transaction(TransactionCodes.WDR, accountNumber, amount, "000", "***"));
 				
 			}
 			
-			if (input == "transfer") //Transferring money
+			if (input.equals("transfer")) //Transferring money
 			{
-				System.out.print("Enter the account that is being transferred from"); //ask for first account
-				String accountNumber1 = System.console().readLine();
+				System.out.println("Enter the account that is being transferred from"); //ask for first account
+				String accountNumber1 = s.nextLine();
 				
-				System.out.print("Enter the account that is being transferred to"); //ask for second account
-				String accountNumber2 = System.console().readLine();
+				System.out.println("Enter the account that is being transferred to"); //ask for second account
+				String accountNumber2 = s.nextLine();
 				
-				System.out.print("Enter the amount to be transferred");
-				String amount = System.console().readLine();
+				System.out.println("Enter the amount to be transferred");
+				String amount = s.nextLine();
 				
-				if (Integer.parseInt(amount) > 100000 && isAgent == false)
-					System.out.print("Sorry, only agents can withdraw more than $1000.00 at once");
-				else if (Integer.parseInt(amount) > 99999999 && isAgent == true)
-					System.out.print("Sorry, not event agents can withdraw more than $999,999.99 at once");
+				if (Integer.parseInt(amount) > 100000 && !isAgent)
+					System.out.println("Sorry, only agents can withdraw more than $1000.00 at once");
+				else if (Integer.parseInt(amount) > 99999999 && isAgent)
+					System.out.println("Sorry, not event agents can withdraw more than $999,999.99 at once");
 				else
 					list.add(new Transaction(TransactionCodes.XFR, accountNumber1, amount, accountNumber2, "***"));
 			}
